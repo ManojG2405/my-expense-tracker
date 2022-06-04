88badcaa1cd4cb0058f6f8dffc56c9f52ef74b48
ManojG2405 <Gawade.manoj77@gmail.com>
2022-06-05 00:51:24 +0530

    Accepting props from ExpenseItem to Expenses

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses.js';
 
 function App() {
   let expenses = [
@@ -32,25 +32,7 @@ function App() {
   return (
     <div className="App">
       <h1>My Expense Tracker</h1>
-      <ExpenseItem
-        date={expenses[0].date}
-        title={expenses[0].title}
-        amount={expenses[0].amount} />
-
-      <ExpenseItem
-        date={expenses[1].date}
-        title={expenses[1].title}
-        amount={expenses[1].amount} />
-
-      <ExpenseItem
-        date={expenses[2].date}
-        title={expenses[2].title}
-        amount={expenses[2].amount} />
-
-      <ExpenseItem
-        date={expenses[3].date}
-        title={expenses[3].title}
-        amount={expenses[3].amount} />
+      <Expenses item={expenses} />
     </div>
   );
 }
