124a035b7cdd2673d6c66595923e76c23dccca9f
ManojG2405 <Gawade.manoj77@gmail.com>
2022-06-07 15:28:19 +0530

    Sending data from Child to parent component (V.16 ) - Successfull

--- src/App.js
+++ src/App.js
@@ -1,41 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Expenses from './components/Expenses/Expenses.js';
 import NewExpense from './components/NewExpenses/NewExpense';
 
-const App = () => {
-  let expenses = [
-    {
-      id: 'e1',
-      title: 'Birthday Party',
-      amount: 95,
-      date: new Date(2022, 5, 5)
-    },
-    {
-      id: 'e2',
-      title: 'Health',
-      amount: 72,
-      date: new Date(2022, 5, 5)
-    },
-    {
-      id: 'e3',
-      title: 'Grosary',
-      amount: 90,
-      date: new Date(2022, 5, 5)
-    },
-    {
-      id: 'e4',
-      title: 'Bike Insurance',
-      amount: 1000,
-      date: new Date(2022, 5, 9)
-    },
-  ];
 
+let DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Birthday Party',
+    amount: 95,
+    date: new Date(2022, 5, 5)
+  },
+  {
+    id: 'e2',
+    title: 'Health',
+    amount: 72,
+    date: new Date(2022, 5, 5)
+  },
+  {
+    id: 'e3',
+    title: 'Grosary',
+    amount: 90,
+    date: new Date(2022, 5, 5)
+  },
+  {
+    id: 'e4',
+    title: 'Bike Insurance',
+    amount: 1000,
+    date: new Date(2022, 5, 9)
+  },
+];
+
+const App = () => {
+  
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+const addExpenseHandler = (expense) =>{
+
+  const updatedExpense = [expense, ...expenses];
+  setExpenses(updatedExpense);
+}
   return (
     <div className="App">
       <h1>My Expense Tracker</h1>
-      <NewExpense />
 
+<NewExpense onAddExpense = {addExpenseHandler} />
       <Expenses item={expenses} />
     </div>
   );
--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,43 +1,32 @@
 import React, { useState } from "react";
 import './ExpenseForm.css'
 
-const ExpenseForm = () => {
-
+const ExpenseForm = (props) => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
-
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
     };
-
     const amountChangeHandler = (event) => {
         setEnteredAmount(event.target.value);
     };
-
     const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value);
     };
-
-
     const submitHandler = (event) => {
         event.preventDefault();
-
-
-
         const expenseData = {
             title: enteredTitle,
             amount: enteredAmount,
             date: new Date(enteredDate)
-        }
-        console.log(expenseData);
+        };
+        props.onSaveExpenseData(expenseData);
         setEnteredTitle('');
         setEnteredAmount('');
         setEnteredDate('');
     };
-
     return (
-
         <form onSubmit={submitHandler}>
             <div className="new-expense_controls">
                 <div className="new-expense_control">
