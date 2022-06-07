5ad1d7ee32a6e9471b031c3e7fa3491cb3aaa684
ManojG2405 <Gawade.manoj77@gmail.com>
2022-06-07 20:56:24 +0530

    App is Ready

--- src/App.js
+++ src/App.js
@@ -5,30 +5,30 @@ import NewExpense from './components/NewExpenses/NewExpense';
 
 
 let DUMMY_EXPENSE = [
-  {
-    id: 'e1',
-    title: 'Birthday Party',
-    amount: 95,
-    date: new Date(2022, 5, 5)
-  },
-  {
-    id: 'e2',
-    title: 'Health',
-    amount: 72,
-    date: new Date(2022, 5, 5)
-  },
-  {
-    id: 'e3',
-    title: 'Grosary',
-    amount: 90,
-    date: new Date(2022, 5, 5)
-  },
-  {
-    id: 'e4',
-    title: 'Bike Insurance',
-    amount: 1000,
-    date: new Date(2022, 5, 9)
-  },
+  // {
+  //   id: 'e1',
+  //   title: 'Birthday Party',
+  //   amount: 1600,
+  //   date: new Date(2022, 5, 5)
+  // },
+  // {
+  //   id: 'e2',
+  //   title: 'Health',
+  //   amount: 4000,
+  //   date: new Date(2022, 5, 5)
+  // },
+  // {
+  //   id: 'e3',
+  //   title: 'Grosary',
+  //   amount: 3700,
+  //   date: new Date(2022, 5, 5)
+  // },
+  // {
+  //   id: 'e4',
+  //   title: 'Bike Insurance',
+  //   amount: 1000,
+  //   date: new Date(2022, 5, 9)
+  // },
 ];
 
 const App = () => {
